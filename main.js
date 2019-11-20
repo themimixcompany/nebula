ba8e5fcaa9f2581f0a717e8337811cf7fee87950
Rommel MARTINEZ <rom@mimix.io>
2019-11-20 16:18:27 +0800

    Use electron-forge; spawn from main.js

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
+const child = require('child_process').execFile;
+const fs = require('fs');
+
+var dirPrefix = "";
+
+if (fs.existsSync("./resources/app")) {
+    dirPrefix="./resources/app/";
+} else {
+    dirPrefix="./";
+}
+
+const enginePath = `${dirPrefix}engine/engine_linux`;
+const engineArgs = [];
+
+const webServerPath = `${dirPrefix}node_modules/local-web-server/bin/cli.js`;
+const webServerArgs = ["-d", `${dirPrefix}app`, "-s", "index.html"];
+
+function loadServers () {
+    child(enginePath, engineArgs, (err, data) => {
+        console.log(err);
+        console.log(data.toString());
+    });
+
+    child(webServerPath, webServerArgs, (err, data) => {
+        console.log(err);
+        console.log(data.toString());
+    });
+}
 
 let mainWindow;
 
@@ -7,26 +35,27 @@ function createWindow () {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        // webPreferences: {
-        //   preload: path.join(__dirname, 'preload.js')
-        // }
     });
 
     mainWindow.loadURL("http://localhost:8000");
-
     // mainWindow.webContents.openDevTools()
 
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', () => {
         mainWindow = null;
     });
 }
 
-app.on('ready', createWindow);
+function runApp () {
+    loadServers();
+    createWindow();
+}
 
-app.on('window-all-closed', function () {
+app.on('ready', runApp);
+
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
     if (mainWindow === null) createWindow();
 });
