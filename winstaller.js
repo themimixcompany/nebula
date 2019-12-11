5c5a672d0099da6579a61976240f5f10cd813a93
The Mimix Company <code@mimix.io>
2019-12-11 21:20:02 +0800

    Add make targets

--- winstaller.js
+++ winstaller.js
@@ -1,10 +1,11 @@
 var electronInstaller = require('electron-winstaller');
 
 resultPromise = electronInstaller.createWindowsInstaller({
-  appDirectory: './',
+  appDirectory: './out/mvp-win32-x64/',
   outputDirectory: './',
   authors: 'Mimix',
-  exe: 'mvp.exe'
+  exe: 'mvp.exe',
+  setupIcon: './logo.ico'
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
