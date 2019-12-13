ec727b7b5712eb4baf139d1783f59b40d4beab6a
Rommel MARTINEZ <rom@mimix.io>
2019-12-13 11:44:30 +0800

    Clean up

--- app/main.js
+++ app/main.js
@@ -1,17 +1,12 @@
 const {app, BrowserWindow} = require('electron');
-
-// const electron = require('electron');
-// const app = require('app');
-// const BrowserWindow = require('browser-window');
-
 const path = require('path');
 const child = require('child_process').execFile;
 const fs = require('fs');
 const express = require('express');
 const appexpress = express();
 const host = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 8000;
-const wsport = process.env.WSPORT || 9797;
+const vport = process.env.VPORT || 8000;
+const wport = process.env.WPORT || 9797;
 const portscanner = require('portscanner');
 var dirPrefix = null;
 
@@ -34,15 +29,9 @@ function setDirPrefix () {
 }
 
 function loadExpress () {
-  portscanner.checkPortStatus(port, host, (error, status) => {
-    if (status == 'closed') {
-      appexpress.use(express.static(`${dirPrefix}/viewer`));
-      appexpress.listen(port, () => {
-        console.log('Express listening on %s:%d', host, port);
-      });
-    } else {
-      console.log(`port ${port} is not available`);
-    }
+  appexpress.use(express.static(`${dirPrefix}/viewer`));
+  appexpress.listen(vport, () => {
+    console.log('Express listening on %s:%d', host, vport);
   });
 }
 
@@ -64,14 +53,14 @@ function loadEngine () {
   var engineArgs = [];
   var child_status = null;
 
-  portscanner.checkPortStatus(wsport, host, (error, status) => {
+  portscanner.checkPortStatus(wport, host, (error, status) => {
     if (status == 'closed') {
       child_status = child(enginePath, engineArgs);
       child_status.stdout.on('data', (data) => {
         console.log(data);
       });
     } else {
-      console.log(`port ${port} is not available`);
+      console.log(`wport ${wport} is not available`);
     }
   });
 }
@@ -84,7 +73,7 @@ function createWindow () {
     height: 600
   });
 
-  mainWindow.loadURL(`http://${host}:${port}`);
+  mainWindow.loadURL(`http://${host}:${vport}`);
   // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
--- makefile
+++ makefile
@@ -1,14 +1,27 @@
-.PHONY: all build install_engine install_viewer save_image linux_package windows_package macos_package
+.PHONY: all clean docker_build install_engine install_viewer save_image linux_package windows_package macos_package
 
 DIR := $(shell basename "$(shell pwd)")
 BASE_NAME = mvp
 IMAGE_NAME = $(BASE_NAME)-${TAG}
 DOCKERFILE = ./Dockerfile
 
-all: build save_image
+all: docker_build save_image
 
-build: install_engine install_viewer
+build:
+	electron-build
+
+docker_build: install_engine install_viewer
 	docker build -f $(DOCKERFILE) -t $(IMAGE_NAME) .
+
+clean:
+	rm -rf node_modules
+	rm -rf out
+
+install:
+	npm install
+
+start:
+	npm start
 
 install_engine:
 	rm -rf app/engine
@@ -19,18 +32,15 @@ install_viewer:
 	rm -rf app/viewer
 	git clone ${VIEWER_SOURCES} app/viewer
 
-save_image:
+docker_save_image:
 	mkdir -p ${RELEASES}/docker/$(BASE_NAME)/${TAG}
 	docker save -o ${RELEASES}/docker/$(BASE_NAME)/${TAG}/docker-$(IMAGE_NAME).tar $(IMAGE_NAME)
 	gzip -f ${RELEASES}/docker/$(BASE_NAME)/${TAG}/docker-$(IMAGE_NAME).tar
 
-run:
+docker_run:
 	docker run -it --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix $(IMAGE_NAME)
 
-clean:
-	rm -rf app/engine
-	rm -rf app/viewer
-	rm -rf out
+packages: clean install linux_package windows_package macos_package
 
 linux_package:
 	electron-forge package --platform=linux
@@ -42,19 +52,10 @@ macos_package:
 	electron-forge package --platform=darwin
 
 linux_installer:
-	electron-forge make --skip-package --platform=linux
-
-windows_installer:
-	electron-forge make --skip-package --platform=win32
-
-macos_installer:
-	electron-forge make --skip-package --platform=macos
-
-appimage:
 	electron-builder --linux --prepackaged out/mvp-linux-x64
 
-nsis:
+windows_installer:
 	electron-builder --windows --prepackaged out/mvp-win32-x64
 
-dmg:
+macos_installer:
 	electron-builder --macos --prepackaged out/mvp-darwin-x64
